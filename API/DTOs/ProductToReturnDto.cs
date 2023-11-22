6005cd41c4bcbba928e7add02078d0bd1be94e89
darius <darius134966@gmail.com>
2023-11-22 10:27:01 +0330

    initialized

---
+++ API/DTOs/ProductToReturnDto.cs
@@ -0,0 +1,21 @@
+namespace API.DTOs
+{
+    public class ProductToReturnDto
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+
+        // table relations
+        public string ProductType { get; set; }
+
+        public string ProductBrand { get; set; }
+    }
+}
